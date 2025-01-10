411060266c49d2cc1cc12afaee60e957a7f2bda4
AnanthuTD <ananthutd2021@gmail.com>
2025-01-11 01:14:28 +0530

    feat: added pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,41 @@ import { WorkspaceModule } from './workspace/workspace.module';
 import { DatabaseModule } from './database/database.module';
 import { KafkaModule } from './kafka/kafka.module';
 import { JwtMiddleware } from './jwt.middleware';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [WorkspaceModule, DatabaseModule, KafkaModule],
+  imports: [
+    WorkspaceModule,
+    DatabaseModule,
+    KafkaModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: 'info', // Adjust log level (e.g., 'debug', 'info', 'warn', 'error')
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true, // Colorize output for better readability in development
+            translateTime: true, // Add timestamps in logs
+            ignore: 'pid,hostname', // Exclude specific fields
+          },
+        },
+        serializers: {
+          req(req) {
+            return {
+              method: req.method,
+              url: req.url,
+              // Exclude sensitive or excessive data from request logs
+            };
+          },
+          res(res) {
+            return {
+              statusCode: res.statusCode,
+            };
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
